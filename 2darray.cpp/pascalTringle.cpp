81b879ebe3a08cb5a6dfc243d3b2fe05206a6d41
iamkaniska <kaniskamaity@gmail.com>
2024-09-01 22:58:25 +0530

    update

---
+++ 2darray.cpp/pascalTringle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+    cout<<"The size Of the vector: ";
+    cin>>n;
+    for (int i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+
+}
--- 2darray.cpp/pascalTringleGeneration.cpp
+++ 2darray.cpp/pascalTringleGeneration.cpp
@@ -8,12 +8,13 @@ int main(){
     for(int i=1;i<=m;i++){
         vector<int>a(i);
         v.push_back(a);
-    }
-    //for generate
+    } 
+    // for generate
     for(int i=0;i<m;i++){
         for(int j=0;j<=i;j++){
             if(j==0 || j==i){
                 v[i][j]=1;
+
             }
             else{
                 v[i][j]=v[i-1][j] + v[i-1][j-1];
@@ -27,5 +28,20 @@ int main(){
         }
         cout<<endl;
     }
+
+
+
+
+
+
+
+
+    
+    // for(int i=0;i<m;i++){
+    //     for(int j=0;j<=i;j++){
+    //         cout<<v[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
 
  } 
