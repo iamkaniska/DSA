aff76b897f40c775ce36d8d7ad3f867c088a4eed
iamkaniska <kaniskamaity@gmail.com>
2024-09-03 00:08:31 +0530

    update

--- 2darray.cpp/pascalTringle.cpp
+++ 2darray.cpp/pascalTringle.cpp
@@ -1,15 +1,34 @@
 #include<iostream>
-#include<vector>
-#include<algorithm>
 using namespace std;
 int main(){
     int n;
-    cout<<"The size Of the vector: ";
     cin>>n;
-    for (int i = 0; i < count; i++)
-    {
-        /* code */
+    int arr[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            if(j==0 || j==i){
+                arr[i][j]=1;
+            }
+            else{
+                arr[i][j]=arr[i-1][j]+arr[i-1][j-1];
+            }
+        }
     }
-    
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<arr[i][j];
+        }
+        cout<<endl;
+    }
 
+    // for specific row
+    int row;
+    cin>>row;
+    if(row<n){
+        for(int i=0;i<=row;i++){
+            cout<<arr[row][i]<<" ";
+        }
+        cout<<endl;
+    }
 }
