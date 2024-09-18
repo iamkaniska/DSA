e553105f1cbb12564b1f9a04bc4c57eed2be4ca4
iamkaniska <kaniskamaity@gmail.com>
2024-09-18 22:59:27 +0530

    update

--- array3/rotateArrayByDplace.cpp
+++ array3/rotateArrayByDplace.cpp
@@ -15,22 +15,15 @@ int main() {
 
     cout << "Enter number of places to rotate (D): ";
     cin >> D;
-
-    // Perform left rotation by D places
     for (int i = 0; i < D; i++) {
-        // Store the first element
         int temp = arr[0];
-
-        // Shift all elements to the left
         for (int j = 0; j < n - 1; j++) {
             arr[j] = arr[j + 1];
         }
 
-        // Move the first element to the end
         arr[n - 1] = temp;
     }
 
-    // Output the rotated array
     cout << "Array after " << D << " left rotations: ";
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
