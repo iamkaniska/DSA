cf4daa7d5d1adbe6a8fedaf2ba4b250958090aff
iamkaniska <kaniskamaity@gmail.com>
2024-10-18 21:58:16 +0530

    update

--- searching/peakIndex.cpp
+++ searching/peakIndex.cpp
@@ -4,5 +4,15 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int low=0;
+    int hi=n-1;
+    while(low<=hi){
+        int mid=low+(hi-low)/2;
+        if()
+    }
     
 }
