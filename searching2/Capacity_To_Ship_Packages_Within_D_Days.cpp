e2d6296ab2f7e0662a34fc703dff6869779756b9
iamkaniska <kaniskamaity@gmail.com>
2024-11-28 01:12:49 +0530

    update

--- searching2/Capacity_To_Ship_Packages_Within_D_Days.cpp
+++ searching2/Capacity_To_Ship_Packages_Within_D_Days.cpp
@@ -1,38 +1,73 @@
-/* 1011. Capacity To Ship Packages Within D Days
- */
-
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    cout<<"Enter array: ";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    int days;
-    cout<<"Enter days: ";
-    cin>>days;
-    //[3,2,2,4,1,4], days = 3
-    int low=0;
-    int hi=0;
-    for(int i=0;i<n;i++){
-            low=max(low,arr[i]);
-            hi=arr[i]+hi;
-    }
-    int ans=0;
-    while(low<=hi){
-        int mid=low+(hi-low)/2;
-        if((mid,days,hi)==true){
-           ans= arr[mid];
-           hi=mid-1;
-        }  
-        else{
-            low=mid+1;
+
+class Solution {
+public:
+    bool check(int mid, vector<int>& weights, int days) {
+        int n = weights.size();
+        int m = mid;
+        int count = 1;
+        for (int i = 0; i < n; i++) {
+            if (m >= weights[i]) {
+                m -= weights[i];
+            } else {
+                count++;
+                m = mid;
+                m -= weights[i];
+            }
         }
+        return count <= days;
     }
-    cout<<ans;
 
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n = weights.size();
+        int maxWeight = 0;
+        int totalWeight = 0;
 
+        for (int i = 0; i < n; i++) {
+            if (maxWeight < weights[i]) {
+                maxWeight = weights[i];
+            }
+            totalWeight += weights[i];
+        }
+
+        int low = maxWeight;
+        int high = totalWeight;
+        int lowCapacity = totalWeight;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (check(mid, weights, days)) {
+                lowCapacity = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return lowCapacity;
+    }
+};
+
+int main() {
+    int n, days;
+    cout << "Enter the number of packages: ";
+    cin >> n;
+
+    vector<int> weights(n);
+    cout << "Enter the weights of the packages: ";
+    for (int i = 0; i < n; i++) {
+        cin >> weights[i];
+    }
+
+    cout << "Enter the number of days: ";
+    cin >> days;
+
+    Solution solution;
+    int result = solution.shipWithinDays(weights, days);
+
+    cout << "Minimum capacity needed to ship packages within " << days << " days is: " << result << endl;
+
+    return 0;
 }
