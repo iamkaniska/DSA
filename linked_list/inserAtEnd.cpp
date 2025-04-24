e6c9bfff43bb4b7b7075d6b6877532d88d15bdad
Kaniska Maity <kaniskamaity@gmail.com>
2025-04-24 18:31:37 +0530

    update

--- linked_list/inserAtEnd.cpp
+++ linked_list/inserAtEnd.cpp
@@ -1,58 +1,61 @@
 #include<iostream>
 using namespace std;
-
-class Node {
-public:
-    int val;
+class Node{
+    public:
+    int data;
     Node* next;
 
-    Node(int val) {
-        this->val = val;
-        this->next = NULL;
+    Node(int data){
+        this->data=data;
+        next=NULL;
     }
 };
+// void display(Node* a){
+//     if(a==NULL) return;
+//         cout<<a->data<<" ";
+//         display(a->next);
+// }
 
-// Function to insert a node at the end
-void insertAtEnd(Node* &head, int value) {
-    Node* newNode = new Node(value);
-
-    if (head == NULL) {
-        head = newNode;
-        return;
+void displayTwo(Node* a){
+    while(a!=NULL){
+        cout<<a->data<<" ";
+        a=a->next;
     }
-
-    Node* temp = head;
-    while (temp->next != NULL) {
-        temp = temp->next;
+    cout<<endl;
+}
+void insertEnd(Node* a, int n){
+    Node* add=new Node(n);
+    while(a->next!=NULL){
+        a=a->next;
     }
-
-    temp->next = newNode;
+    a->next=add;
+};
+void insertHead(Node*& head, int data) {
+    Node* newNode = new Node(data);
+    newNode->next = head;
+    head = newNode; // Update the actual head pointer
 }
 
-// Function to print the linked list
-void printList(Node* head) {
-    Node* temp = head;
 
-    while (temp != NULL) {
-        cout << temp->val << " -> ";
-        temp = temp->next;
-    }
-
-    cout << "NULL" << endl;
+int main(){
+    Node* a=new Node(10);
+    Node* b=new Node(20);
+    Node* c=new Node(30);
+    Node* d=new Node(40);
+    Node* e=new Node(50);
+    Node* f=new Node(60);
+    a->next=b;
+    b->next=c;
+    c->next=d;
+    d->next=e;
+    e->next=f;
     
-}
+    displayTwo(a);
+    // insertEnd(a,90);
+    // displayTwo(a);
+    insertHead(a,80);
+    displayTwo(a);
 
-int main() {
-    Node* head = NULL;
+    
 
-    insertAtEnd(head, 5);
-    insertAtEnd(head, 10);
-    insertAtEnd(head, 15);
-    insertAtEnd(head, 7);
-    insertAtEnd(head, 25);
-    insertAtEnd(head, 35);
-
-    printList(head); // Output: 5 -> 10 -> 15 -> NULL
-
-    return 0;
 }
