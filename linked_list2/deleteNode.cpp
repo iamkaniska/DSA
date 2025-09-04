b4ab37c32fd175d3cf198e8e632ff5e7411db9da
Kaniska Maity <kaniskamaity@gmail.com>
2025-09-04 16:27:27 +0530

    update

--- linked_list2/deleteNode.cpp
+++ linked_list2/deleteNode.cpp
@@ -1,5 +1,5 @@
 /* delete Node from a linkedlist */
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 class Node{
     public:
@@ -53,5 +53,46 @@ int main(){
     Deletenode(a,2);
     display(a);
 
+
+} */
+
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data){
+        this->data=data;
+        next=NULL;
+    }
+};
+void display(Node* head){
+    while(head!=NULL){
+        head=head->next;
+        cout<<head->data<<" ";
+    }
+    cout<<endl;
+}
+void deleteNode(Node* temp){
+    temp->data=temp->next->data;
+    temp->next=temp->next->next;
+}
+int main(){
+    Node* a= new Node(10);
+    Node* b= new Node(20);
+    Node* c= new Node(30);
+    Node* d= new Node(40);
+    Node* e= new Node(50);
+    Node* f= new Node(60);
+    a->next=b;
+    b->next=c;
+    c->next=d;
+    d->next=e;
+    e->next=f;
+    f->next=NULL;
+    display(a);
+    deleteNode(b);
+    display(a);
 
 }
